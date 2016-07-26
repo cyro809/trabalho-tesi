fb502f5e91640863313072665a1ae42a0f62854d
Cyro Guimaraes <cyro809@gmail.com>
2016-07-26 14:26:24 -0300

    Comentarios no utils.py do web_crawler

--- web_crawler/utils.py
+++ web_crawler/utils.py
@@ -5,11 +5,21 @@ import codecs
 import fileinput
 import json
 
+# ---------------------------------------------------------------------------
+# Função write_json(): Escreve o arquivo json com a lista de dicionários
+#                      com as músicas
+# - filename: Nome do arquivo que será escrito
+# - lyrics_array: Lista de dicionários com as informações da músicas
+# ---------------------------------------------------------------------------
 def write_json(filename, lyrics_array):
     with codecs.open(filename, 'w', encoding='utf-8') as fp:
         json.dump(lyrics_array, fp, ensure_ascii=False, indent=4, separators=(',', ': '))
 
 
+# ---------------------------------------------------------------------------
+# Função read_json(): Lê o arquivo json com as informações das músicas
+# - filename: Nome do arquivo json que será lido
+# ---------------------------------------------------------------------------
 def read_json(filename):
     try:
         with codecs.open(filename, 'r', encoding='utf-8') as fp:
@@ -18,17 +28,19 @@ def read_json(filename):
     except IOError:
         return []
 
-
+# ---------------------------------------------------------------------------
+# Função read_urls_file(): Lê o arquivo com as urls e retorna uma lista de urls
+# ---------------------------------------------------------------------------
 def read_urls_file(filename):
     urls_array = []
     for url in fileinput.input(filename):
         url = url.strip()
         urls_array.append(url)
-    print '###########################################################'
-    print urls_array
-    print '###########################################################'
     return urls_array
 
+# ---------------------------------------------------------------------------
+# Função read_urls_input(): Le as urls através do terminal ou linha de comando
+# ---------------------------------------------------------------------------
 def read_urls_input():
     urls_array = []
     urls = raw_input('Insert the lyrics url separated by ",": \n')
