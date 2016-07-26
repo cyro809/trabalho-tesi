994298ca63ae3fd19f7740c9ed8b9df480b525b7
hugodovs <hugodovs@gmail.com>
2016-07-26 16:12:04 -0300

    tfIdf 100%

--- sentimusic/tfidf/tfidf_matrix.py
+++ sentimusic/tfidf/tfidf_matrix.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import tfidf_helpers as f
+#Import:
 import json
 import numpy as np
+import tfidf_helpers as f
 
+#Pass the filename with documents and I will give you tf_idf_matrix
 def generate_matrix(filename):
+    
     #Open corpus (Open File from JSON):
     with open("../%s"%filename) as data_file:    
         data = json.load(data_file)
@@ -12,10 +15,8 @@ def generate_matrix(filename):
     #Create a list of words (Our vocabulary) and prepare the documents:
     vocabulary = f.build_lexiconAndDocuments(data)
     
-    
     music_dict = {}
     music_list = []
-    #Make doc_tf_vector in docList:
     doc_term_matrix = []
     for i in range(0,len(data)):
         doc_vector = [f.freq(word, i) for word in vocabulary]
@@ -26,13 +27,10 @@ def generate_matrix(filename):
         music_list.append(data[i]['url'])
         music_dict[data[i]['url']] = {'pos': i, 'sentiment': data[i]['sentiment']}
         
-    #Transform in numpy matrix:
-    doc_term_matrix = np.matrix(doc_term_matrix)
-
+    #Make TF:IDF matrix
+    tfIdf_matrix = f.docTerm_to_tfIdfMatrix(doc_term_matrix)
+    
     return (doc_term_matrix, vocabulary, music_list, music_dict)
 #----------------------------------------------
-
 #Normalize vector:
 #doc_term_matrix_normalized = f.normalizeMatrix(doc_term_matrix)
-
-#print(doc_term_matrix_normalized)
